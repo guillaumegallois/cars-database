7e9db71052f010476b1c8e86dcca68293a4d4111
Guillaume Gallois <guillaume@guillaumegallois.com>
2025-03-20 15:04:25 +0100

    Spring boot Modulith app

---
+++ src/main/java/com/guillaumegallois/carsdatabase/CarsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.guillaumegallois.carsdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guillaumegallois/carsdatabase/CarsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillaumegallois.carsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
