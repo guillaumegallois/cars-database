103bb06d99f35e044872d341f5dde26987a2e34c
Guillaume Gallois <guillaume@guillaumegallois.com>
2025-03-21 17:00:34 +0100

    Modulith Test App

--- src/test/java/com/guillaumegallois/carsdatabase/CarsDatabaseApplicationTests.java
+++ src/test/java/com/guillaumegallois/carsdatabase/CarsDatabaseApplicationTests.java
@@ -2,12 +2,29 @@ package com.guillaumegallois.carsdatabase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class CarsDatabaseApplicationTests {
 
+	ApplicationModules modules = ApplicationModules.of(CarsDatabaseApplication.class);
+
 	@Test
-	void contextLoads() {
+	void verifiesModularStructure() {
+		modules.verify();
+	}
+
+	@Test
+	void createModuleDocumentation() {
+		new Documenter(modules)
+				.writeDocumentation()
+				.writeIndividualModulesAsPlantUml();
+	}
+
+	@Test
+	void createApplicationModuleModel() {
+		modules.forEach(System.out::println);
 	}
 
 }
